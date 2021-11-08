5291116322b4d4febfecadeccbd6675b66694d9f
heewon0915 <heewon0915@naver.com>
2021-11-08 22:14:26 +0900

    로그인 화면

--- front-end/src/components/Login.vue
+++ front-end/src/components/Login.vue
@@ -1,27 +1,26 @@
 <template>
-  <form class="row g-3">
-  <div class="row">
+<div>
+ 
+  <div class="row mb-3">
   <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">ID</label>
   <div class="col-sm-10">
     <input type="email" class="form-control form-control-lg" id="colFormLabelLg" placeholder="아이디 입력">
   </div>
-  </div> 
-  <div class="col-md-6">
-    <label for="inputPassword4" class="form-label">PASSWORD</label>
-    <input type="password" class="form-control" id="inputPassword4">
   </div>
-  <div class="col-12">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
+  <div class="row mb-3">
+  <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">PASSWORD</label>
+  <div class="col-sm-10">
+    <input type="email" class="form-control form-control-lg" id="colFormLabelLg" placeholder="비밀번호 입력">
   </div>
-  <div class="col-12">
-    <button type="submit" class="btn btn-primary">Sign in</button>
   </div>
-  </form>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">아이디 저장하기</label>
+  </div>
+  <div class="d-grid gap-2 col-6 mx-auto">
+  <button class="btn btn-primary" type="button">로그인</button>
+</div>
+</div>
 </template>
 
 <script>
@@ -31,5 +30,8 @@ export default {
 </script>
 
 <style>
+.login{
+  margin-top: 100px;
+}
 
 </style>
