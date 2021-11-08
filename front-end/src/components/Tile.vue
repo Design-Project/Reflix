cda61046ed126923d7bd9685b0575d2777d8ee43
cjy3082 <cjy3082@naver.com>
2021-11-08 21:19:37 +0900

    계정화면 타일배열 component

---
+++ front-end/src/components/Tile.vue
@@ -0,0 +1,66 @@
+<template>
+<div>
+
+  <div class="parent" style="width: 100%; height: 150px; background: #585858;">
+    <div class="child" style="background: #2E9AFE;">
+            <img src="https://movie-phinf.pstatic.net/20211026_97/1635213484861yCnwO_JPEG/movie_image.jpg
+" class="tile-image" alt="...">
+    </div>
+    <div class="child" style="background: #FA5858;">
+        <img src="https://movie-phinf.pstatic.net/20210915_104/1631681279096sdjNA_JPEG/movie_image.jpg" class="tile-image" alt="...">
+    </div>
+    <div class="child" style="background: #81F781;">
+        <img src="https://movie-phinf.pstatic.net/20210929_275/1632903680953494p8_JPEG/movie_image.jpg" class="tile-image" alt="...">
+    </div>
+</div>
+
+
+  <div class="parent" style="width: 100%; height: 150px; background: #585858;">
+    <div class="child" style="background: #2E9AFE;">
+            <img src="https://movie-phinf.pstatic.net/20210930_23/1632986239409z0czk_JPEG/movie_image.jpg
+" class="tile-image" alt="...">
+    </div>
+    <div class="child" style="background: #FA5858;">
+        <img src="https://movie-phinf.pstatic.net/20211013_94/1634115796140K407h_JPEG/movie_image.jpg" class="tile-image" alt="...">
+    </div>
+    <div class="child" style="background: #81F781;">
+        <img src="https://movie-phinf.pstatic.net/20211027_123/1635324527597B4R9C_JPEG/movie_image.jpg" class="tile-image" alt="...">
+    </div>
+</div>
+
+  <div class="parent" style="width: 100%; height: 150px; background: #585858;">
+    <div class="child" style="background: #2E9AFE;">
+            <img src="https://movie-phinf.pstatic.net/20210917_60/16318534287592XLg8_JPEG/movie_image.jpg
+" class="tile-image" alt="...">
+    </div>
+    <div class="child" style="background: #FA5858;">
+        <img src="https://movie-phinf.pstatic.net/20211022_30/1634889034704rEGxK_JPEG/movie_image.jpg" class="tile-image" alt="...">
+    </div>
+    <div class="child" style="background: #81F781;">
+        <img src="https://movie-phinf.pstatic.net/20211001_121/16330536363187liBB_JPEG/movie_image.jpg" class="tile-image" alt="...">
+    </div>
+</div>
+
+
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.parent {
+    display: flex;
+}
+.child {
+    flex: 1;
+}
+.tile-image {
+    width:100%;
+    height:100%;
+    object-fit:cover;
+}
+</style>
