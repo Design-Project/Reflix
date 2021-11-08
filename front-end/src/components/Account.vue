cdba655b684182107c0586b6ed9591ab244066d1
heewon0915 <heewon0915@naver.com>
2021-11-08 21:19:34 +0900

    Account 화면 생성

---
+++ front-end/src/components/Account.vue
@@ -0,0 +1,176 @@
+<template>
+  <div>
+    <div class="top">
+      <a class="accountName">{{ 계정이름 }}님의 reflix</a>
+    </div>
+
+    <div class="account">
+      <img src="../assets/image.png" class="accimage" />
+      <!-- 계정이미지 불러오기 -->
+    </div>
+    <h4 style="margin-left: 190px">{{ 계정이름 }}</h4>
+      <!--계정이름 불러오기 -->
+
+    <div>
+      <p style="margin-left:200px">{{나이}} / {{성별}} </p>
+    </div>
+    <div class="row">
+      <div class="col-4" > <a class="mypick">My Pick! </a>  </div>
+      <div class="col-2" > <h7 class="picktext">액션</h7> </div>
+      <div class="col-2"> <h7 class="picktext">멜로</h7> </div>
+      <div class="col-2"> <h7 class="picktext">판타지</h7> </div>
+    </div>
+   
+
+
+
+  <div>
+    <div class="follow">
+      <!-- 팔로우 버튼 생성 -->
+      <div class="dropdown">
+        <a
+          class="btn btn-secondary dropdown-toggle"
+          href="#"
+          role="button"
+          id="dropdownMenuLink"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          팔로우
+        </a>
+
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+          <li><a class="dropdown-item" href="#">팔로우하기</a></li>
+          <li><a class="dropdown-item" href="#">팔로우 취소</a></li>
+          <li><a class="dropdown-item" href="#">차단하기</a></li>
+        </ul>
+      </div>
+    </div>
+
+
+    <div class="row">
+      <p class="col-2" style="margin-left:130px"> <mark>팔로우 </mark> </p>
+      <p class="col-2"> <mark>팔로잉 </mark> </p>
+      <p class="col-2"> <mark>게시물 </mark> </p>
+    </div>
+    <div class="row">
+      <h4 class="col-2" style="margin-left:135px"> <mark>362 </mark> </h4>
+      <h4 class="col-2"> <mark>331 </mark> </h4>
+      <h4 class="col-2" style="margin-left:10px"> <mark>  3 </mark> </h4>
+    </div>
+    
+
+    <div class="rank">
+      <h7 class="ranktext">{{계정이름}}님의 평점 랭킹</h7>
+    </div>
+
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">순위</th>
+          <th scope="col">제목</th>
+          <th scope="col">종류</th>
+          <th scope="col">평점</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>종이의집</td>
+          <td>스페인드라마</td>
+          <td>9.67 / 10.00 </td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>기생충</td>
+          <td>한국영화</td>
+          <td>9.55 / 10.00</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>귀멸의 칼날 </td>
+          <td>애니메이션</td>
+          <td>9.42 / 10.00</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div class="bottom"></div>
+
+   
+
+
+      
+  </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Accont",
+  data() {
+    return {
+      계정이름: "dahlia_hee",
+      나이: "20대",
+      성별: "여자",
+    }
+  }
+}
+</script>
+
+<style>
+.picktext{
+  background-color: cornflowerblue;
+  color: white;
+  margin-left: 10px;
+}
+
+.mypick{
+  color: darkslateblue;
+  margin-left: 80px;
+  margin-bottom: 10px;
+}
+
+.bottom{
+  background-color: darkslateblue;
+  margin-top: 20px;
+  padding: 20px;
+
+}
+.rank{
+  background-color: darkslateblue;
+  padding: 3px;
+  border-radius: 20px;
+  margin-left: 100px;
+  margin-right: 100px;
+  margin-top:10px;
+}
+.ranktext{
+  color: white;
+  margin-left: 50px;
+  padding: 5px;
+}
+.follow {
+  margin-left: 200px;
+}
+.top {
+  background-color: darkslateblue;
+  padding: 15px;
+}
+.accountName {
+  color: white;
+  padding: 10px;
+  margin-left: 10px;
+}
+.accimage {
+  width: 30%;
+  margin-right: 500px;
+  border-radius: 50px;
+}
+.account {
+  margin-left: 200px;
+  margin-top: 50px;
+}
+</style>
---
+++ front-end/src/components/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <form class="row g-3">
+  <div class="row">
+  <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">ID</label>
+  <div class="col-sm-10">
+    <input type="email" class="form-control form-control-lg" id="colFormLabelLg" placeholder="아이디 입력">
+  </div>
+  </div> 
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">PASSWORD</label>
+    <input type="password" class="form-control" id="inputPassword4">
+  </div>
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Check me out
+      </label>
+    </div>
+  </div>
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Sign in</button>
+  </div>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
